f2fea97b986007050b2d90068a24e5d36bc024ae
rodri2580 <rodri230998@gmail.com>
2018-04-28 04:50:45 -0500

    Working

---
+++ Capturador_de_pedidos/Capturador_de_pedidos/Controlador/ControladorUsuario.cs
@@ -0,0 +1,41 @@
+﻿using Capturador_de_pedidos.BD;
+using Capturador_de_pedidos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Capturador_de_pedidos.Controlador
+{
+    class ControladorUsuario
+    {
+        public String Login(String nick, String pwd)
+        {
+            String name = "";
+            Conexion conn = new Conexion();
+            DataTableReader reader = null;
+            List<SqlParameter> _Parametros = new List<SqlParameter>();
+            conn.Conectar();
+            _Parametros.Add(new SqlParameter("@Usuario", nick));
+            _Parametros.Add(new SqlParameter("@Password", pwd));
+            conn.PrepararProcedimiento("dbo.sp_Login", _Parametros);
+            reader = conn.EjecutarTableReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                name += reader["Nombre"].ToString() + " " + reader["Apellido"].ToString();
+                reader.Close();
+            }
+            else
+            {
+                MessageBox.Show("Datos incorrectos", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+            return name;
+        }
+    }
+}
---
+++ Capturador_de_pedidos/Capturador_de_pedidos/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Capturador_de_pedidos.Controlador;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Capturador_de_pedidos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            ControladorUsuario login = new ControladorUsuario();
+            if (login.Login(txtUser.Text, pwdUser.Password.ToString()) != "")
+            {
+                Vista.Menu OM = new Vista.Menu(login.Login(txtUser.Text, pwdUser.Password.ToString()));
+                OM.Show();
+                this.Close();
+            }
+        }
+    }
+}
